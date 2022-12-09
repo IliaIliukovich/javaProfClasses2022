efea22d7b9b9d065a4479df2ff5cb7f090cfbd32
Ilia Iliukovich <ilia.iliukovich@gmail.com>
2022-12-09 14:49:50 +0100

    Tasks with arrary and loops

---
+++ basic_43_44m/src/lesson09122022/arrays/TaskPow.java
@@ -0,0 +1,33 @@
+package lesson09122022.arrays;
+
+public class TaskPow {
+
+//    Создать метод для возведения в степень числа и *извлечения корня 3 степени из числа.
+    public static void main(String[] args) {
+
+        double z = powerMethod(3, 4);
+        double sqrt = sqrt3Method(3);
+        double sqrt2 = sqrt3Method(4);
+        double sqrt3 = sqrt3Method(27);
+        double sqrt4 = sqrt3Method(81);
+
+        System.out.println("Input: z = " + z);
+        System.out.println("Input: sqrt = " + sqrt);
+        System.out.println("Input: sqrt = " + sqrt2);
+        System.out.println("Input: sqrt = " + sqrt3);
+        System.out.println("Input: sqrt = " + sqrt4);
+
+    }
+
+    private static double sqrt3Method(int x) {
+        double z = 1.0/3.0;
+        return Math.pow(x, z);
+    }
+
+    private static double powerMethod(int x, int y) {
+
+        return Math.pow(x, y);
+
+    }
+
+}
